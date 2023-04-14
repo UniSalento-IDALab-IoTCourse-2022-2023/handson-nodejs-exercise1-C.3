32a0bf23196e4053301662e03165a13a363853fc
tmontanaro <teodoro.montanaro@unisalento.it>
2023-04-15 01:14:46 +0200

    Working project

--- serverApp.js
+++ serverApp.js
@@ -1,6 +1,18 @@
 var express = require("express");
 const MongoClient = require('mongodb').MongoClient;
+const WebSocket = require('ws');
+const path = require('path');
+
 const uri = 'mongodb://130.192.137.1/TemperatureDB';
+
+const wss = new WebSocket.Server({ port: 3001 });
+
+wss.on('connection', function connection(ws) {
+    ws.on('message', function incoming(message) {
+        console.log('received: %s', message);
+    });
+    ws.send('something');
+});
 
 var app = express();
 app.listen(3000, () => {
@@ -44,12 +56,20 @@ app.post("/temperature", (req, res, next) => {
     }
 
     run().catch(console.dir);
+    async function pushToClient(){
+        wss.clients.forEach(function each(client) {
+            if (client.readyState === WebSocket.OPEN) {
+                client.send(temperature);
+            }
+        });
+    }
+    pushToClient().catch(console.dir);
     res.sendStatus(200);
 });
 
 
 app.get('/dashboard', async (req, res) => {
-
+    /*
     const client = new MongoClient(uri, { useUnifiedTopology: true });
     async function run() {
         try {
@@ -79,4 +99,6 @@ app.get('/dashboard', async (req, res) => {
     var finalTemp = await run().catch(console.dir);
     console.log(finalTemp);
     res.send('Hello World! The last temperature is: '+finalTemp);
+     */
+    res.sendFile(path.join(__dirname + '/index.html'));
 })
